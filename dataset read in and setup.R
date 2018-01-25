61c77274c6f9c37c6c6d5ad606db8a99321c00c0
Randall Boyes <randy.boyes@gmail.com>
2018-01-25 13:22:03 -0500

    split setup into own file

---
+++ dataset read in and setup.R
@@ -0,0 +1,22 @@
+library(lme4)
+library(readr)
+
+HBSC_data <- read_csv("C:/Users/Student/Desktop/Data/HBSC Sport and Substance BMI/HBSC data.csv")
+
+d <- as.data.frame(na.omit(HBSC_data))
+
+d$weed <- as.integer(d$weed)
+d$sports <- as.integer(d$sports)
+d$team <- as.integer(d$team)
+d$ind <- d$sports - 2*d$team
+
+d$bmifactor <- as.factor(d$`bmi classification`)
+m <- dummy(d$bmifactor)
+mdf <- as.data.frame(m)
+mdf$any_thin <- or(mdf$`severly thin`, mdf$thin)
+d <- cbind(d,m)
+
+db <- d[d$Gender == "Boy",]
+dg <- d[d$Gender == "Girl",]
+
+
