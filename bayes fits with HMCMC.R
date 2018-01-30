c24ee0c737b1c7d8317c548be7d1200eca7dcc0c
Randall Boyes <randy.boyes@gmail.com>
2018-01-30 11:27:18 -0500

    converging team/ind bayesian models

--- bayes fits with HMCMC.R
+++ bayes fits with HMCMC.R
@@ -1,6 +1,9 @@
 library(devtools)
+install.packages("rstan")
 
 devtools::install_github("rmcelreath/rethinking", force = TRUE)
+
+library(rethinking)
 
 #Bad model. Doesn't work at all
 
@@ -30,23 +33,26 @@ m1.3 <- map2stan(
 
 precis(m1.3)
 
-#Adds a unique conseq. numeric ID variable for each school in the data set 
-
-db$SchoolID <- as.numeric(factor(db$School, levels = unique(db$School)))
-
+#Model 1.4
 #Adaption to multilevel model with random intercept vector for schools (length = 192)
+#Priors for all betas set with normal distributions with mean 0 and stdev 10
+#Prior for standard deviation of normal distribution of random school intercepts set with a regularizing cauchy distribution
 
 m1.4 <- map2stan(
   alist(
     binge ~ dbinom(1,p),
-    logit(p) <- a + a_school[SchoolID] + bTeam*team +
-    + team*overweight*bOverInt + overweight*bOver
-    + team*obese*bObeseInt + obese*bObese,
+    logit(p) <- a 
+    + a_school[SchoolID] 
+    + bTeam*team
+    + team*overweight*bOverInt 
+    + overweight*bOver
+    + team*obese*bObeseInt 
+    + obese*bObese,
     a_school[SchoolID] ~ dnorm(0,sigma_school),
     c(a,bTeam,bObeseInt,bObese,bOverInt,bOver) ~ dnorm(0,10),
     sigma_school ~ dcauchy(0,1)
   ), 
-  data = db)
+  data = db2)
 
 precis(m1.4)
 plot(m1.4)
@@ -54,17 +60,56 @@ plot(precis(m1.4))
 
 post <- extract.samples(m1.4)
 
-#Adds thin people back in, convergence questionable
+#Model 1.5
+#Starts with 1.4, adds interaction with individual sports. Increases time for MCMC convergence by approximately 1.5x
 
 m1.5 <- map2stan(
   alist(
     binge ~ dbinom(1,p),
-    logit(p) <- a + a_school[SchoolID] + bTeam*team +
-      + team*overweight*bOverInt + overweight*bOver
-    + team*obese*bObeseInt + obese*bObese,
-    + team*any_thin*bThinInt + bThin*any_thin,
+    logit(p) <- a 
+    + a_school[SchoolID] 
+    + bTeam*team
+    + bInd*ind
+    + bSportInt*team*ind
+    + bObese*obese
+    + bTeamObeseInt*team*obese 
+    + bIndObeseInt*ind*obese
+    + bSportObeseInt*team*ind*obese
+    + bOver*overweight
+    + bTeamOverInt*team*overweight 
+    + bIndOverInt*ind*overweight
+    + bSportOverInt*team*ind*overweight,
     a_school[SchoolID] ~ dnorm(0,sigma_school),
-    c(a,bTeam,bObeseInt,bObese,bOverInt,bOver,bThin,bThinInt) ~ dnorm(0,10),
+    c(a,bTeam,bInd,bSportInt,bObese,bTeamObeseInt,bIndObeseInt,bSportObeseInt,bOver,bTeamOverInt,bIndOverInt,bSportOverInt) ~ dnorm(0,10),
     sigma_school ~ dcauchy(0,1)
   ), 
-  data = db)
+  data = db2)
+
+precis(m1.5)
+
+#Model 2.0
+#Starts with 1.5, adds confounding variables
+
+m2.0 <- map2stan(
+  alist(
+    binge ~ dbinom(1,p),
+    logit(p) <- a 
+    + a_school[SchoolID] 
+    + bTeam*team
+    + bInd*ind
+    + bSportInt*team*ind
+    + bObese*obese
+    + bTeamObeseInt*team*obese 
+    + bIndObeseInt*ind*obese
+    + bSportObeseInt*team*ind*obese
+    + bOver*overweight
+    + bTeamOverInt*team*overweight 
+    + bIndOverInt*ind*overweight
+    + bSportOverInt*team*ind*overweight,
+    a_school[SchoolID] ~ dnorm(0,sigma_school),
+    c(a,bTeam,bInd,bSportInt,bObese,bTeamObeseInt,bIndObeseInt,bSportObeseInt,bOver,bTeamOverInt,bIndOverInt,bSportOverInt) ~ dnorm(0,10),
+    sigma_school ~ dcauchy(0,1)
+  ), 
+  data = db2)
+
+precis(m2.0)
--- dataset read in and setup.R
+++ dataset read in and setup.R
@@ -1,8 +1,10 @@
 library(readr)
+library(lme4)
+library(dplyr)
 
 HBSC_data <- read_csv("C:/Users/Student/Desktop/Data/HBSC Sport and Substance BMI/HBSC data.csv")
 
-d <- as.data.frame(na.omit(HBSC_data))
+d <- as.data.frame(HBSC_data)
 
 d$weed <- as.integer(d$weed)
 d$sports <- as.integer(d$sports)
@@ -12,9 +14,17 @@ d$ind <- d$sports - 2*d$team
 d$bmifactor <- as.factor(d$`bmi classification`)
 m <- dummy(d$bmifactor)
 mdf <- as.data.frame(m)
-mdf$any_thin <- mdf$`severly thin` | mdf$thin
-d <- cbind(d,m)
+mutate(mdf, any_thin = xor(thin, `severely thin`))
+d <- cbind(d,mdf)
 
 dg <- d[d$Gender == "Girl",]
 
-db <- na.omit(d[d$Gender == "Boy",c("team","ind","binge","obese","overweight","any_thin","School")])
+db <- na.omit(d[d$Gender == "Boy",])
+
+db2 <- db[,c("team","ind","binge","obese","overweight","any_thin","School")]
+dg2 <- dg[,c("team","ind","binge","obese","overweight","any_thin","School")]
+
+#Adds a unique conseq. numeric ID variable for each school in the data set 
+
+db2$SchoolID <- as.numeric(factor(db2$School, levels = unique(db$School)))
+dg2$SchoolID <- as.numeric(factor(dg2$School, levels = unique(db$School)))
